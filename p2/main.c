ee9be0c91e83fb8b2f5dc48abdd3e61dd8bc8a40
Shin YoungSub <147918555+whiteys1@users.noreply.github.com>
2026-05-25 16:52:40 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -2,11 +2,12 @@
 
 int main(){
 	int N;
-
-	scanf("%d", &N);  // 변수 선언들 모두 끝난 후에 scanf 받으면 좋겠습니다.
 	int arr_1[20] = {};
 	int arr_2[20] = {};
-	int *p, *q;   //nullptr 초기화하길 바랍니다.
+	int result[20] = {};
+	int *p = nullptr, *q = nullptr, *r = nullptr;   //nullptr 초기화하길 바랍니다.
+
+	scanf("%d", &N);
 
 	for(p = arr_1; p < arr_1 + N; p++){
 		scanf("%d", p);
@@ -18,11 +19,14 @@ int main(){
 
 	p = arr_1;
 	q = arr_2 + N - 1;
-	
-	for(int i = 0; i < N; i++){                            //포인터로 for문 돌려야 합니다. 이 부분 수정해서 다시 메일 보내주세요! 차라리 결과값을 저장하는 배열 하나 
-		printf(" %d", *(p + i) + *(q - i));                // 더 추가해서 코드 작성하는 것이 좋을 것 같습니다.  
+
+	for(r = result; r < result + N; r++, p++, q--){
+		*r = *p + *q;
 	}
 
-
+	for(r = result; r < result +  N; r++){
+		printf(" %d", *r);
+	}
+	
 	return 0;
 }
