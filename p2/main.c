3d8912631dc2efac5a121a650e769c73837fda49
Shin YoungSub <147918555+whiteys1@users.noreply.github.com>
2026-05-25 14:51:29 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -5,18 +5,19 @@ int main(){
 
 	scanf("%d", &N);
 	int arr_1[20] = {};
-	int arr_2[20] ={};
+	int arr_2[20] = {};
+	int *p, *q;
 
-	for(int *p = arr_1; p < arr_1 + N; p++){
+	for(p = arr_1; p < arr_1 + N; p++){
 		scanf("%d", p);
 	}
 
-	for(int *q = arr_2; q < arr_2 + N; q++){
+	for(q = arr_2; q < arr_2 + N; q++){
 		scanf("%d", q);
 	}
 
-	int *p = arr_1;
-	int *q = arr_2 + N - 1;
+	p = arr_1;
+	q = arr_2 + N - 1;
 	
 	for(int i = 0; i < N; i++){
 		printf("%d ", *(p + i) + *(q - i));
